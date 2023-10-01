4ecf5fb362acf06ed0c8cbfefa7d5ad66be158a3
John Butler <jbutler@tripadvisor.com>
2023-10-01 15:29:10 -0400

    multiple entrypoints

---
+++ src/geometry/index.ts
@@ -0,0 +1,7 @@
+import { Angle } from './Angle';
+import { Coordinates } from './Coordinates';
+import { Dimensions } from './Dimensions';
+import { Rect } from './Rect';
+import { Vector } from './Vector';
+
+export type { Angle, Coordinates, Dimensions, Rect, Vector };
