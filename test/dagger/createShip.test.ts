2d9debf23c3049f1a79c9b9703ea0f3e34a68f63
John Butler <jbutler@tripadvisor.com>
2023-10-01 14:44:20 -0400

    fix tests

--- test/dagger/createShip.test.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from 'vitest';
-import { createPlayerShip } from '../../src/dagger/playerShip';
-import { Coordinates } from '../../src/geometry/Coordinates';
-
-test('ship', async () => {
-  const ship = await createPlayerShip({ centerCoordinates: Coordinates.zero() });
-  expect(ship.getName()).toBe('ship');
-  expect(ship.getCenterCoordinates()).toEqual(Coordinates.zero());
-});
