93739e69dfb0d5222b5aec56d271d9434154bdaf
John Butler <jbutler@tripadvisor.com>
2023-10-04 16:59:35 -0400

    add exports for metrics

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,7 @@
-import { checkNotNull, check } from './preconditions';
+import { check, checkNotNull } from './preconditions';
 import * as Arrays from './Arrays';
 import * as Random from './Random';
 import { getCurrentTimeSeconds } from './getCurrentTimeSeconds';
+import { Metric, MetricType } from './Metric';
 
-export { Arrays, Random, check, checkNotNull, getCurrentTimeSeconds };
+export { Arrays, Random, check, checkNotNull, getCurrentTimeSeconds, Metric, MetricType };
