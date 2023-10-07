6884f5aa3be411d24172f29cf241f96c35ced473
John Butler <jbutler@tripadvisor.com>
2023-10-07 16:58:26 -0400

    export event types

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 import { Engine } from './core/Engine';
 import { Scene } from './core/Scene';
 import { init } from './core/init';
-import { GlobalScript } from './events/GlobalScript';
-import { TickEvent } from './events/TickEvent';
 import { Camera } from './geometry/Camera';
 import { Keyboard, HeldKey } from './input/Keyboard';
 
 export { Camera, Engine, Keyboard, Scene, init };
-export type { GlobalScript, TickEvent, HeldKey };
+export type { HeldKey };
