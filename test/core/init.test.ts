6d80d3f29e9b65bb46417da26f8a1f648a77ec17
John Butler <jbutler@tripadvisor.com>
2023-11-04 11:34:35 -0400

    update init()

--- test/core/init.test.ts
+++ test/core/init.test.ts
@@ -1,12 +1,10 @@
-import { Engine, init } from '../../src';
+import { Camera, Engine, init, Scene } from '../../src';
 import { test, expect, vi } from 'vitest';
 
 test('init', async () => {
   const container = {
     appendChild: () => {}
   } as unknown as HTMLElement;
-  const viewportDimensions = { width: 100, height: 100 };
-  const sceneDimensions = { width: 200, height: 200 };
   const mockWindow = vi.stubGlobal('window', {
     addEventListener: () => {}
   });
@@ -22,13 +20,26 @@ test('init', async () => {
     });
   }
   const mockAudioContext = vi.stubGlobal('AudioContext', MockAudioContext);
+
+  const viewportDimensions = { width: 100, height: 100 };
+  const scene = Scene.create({
+    name: 'scene',
+    dimensions: { width: 200, height: 200 },
+    camera: Camera.create({
+      centerCoordinates: {
+        x: viewportDimensions.width / 2,
+        y: viewportDimensions.height / 2
+      },
+      dimensions: viewportDimensions
+    })
+  });
   const engine: Engine = await init({
     container,
     viewportDimensions,
-    sceneDimensions
+    scenes: [scene],
+    initialScene: 'scene'
   });
-  const scene = engine.getScene();
-  expect(scene.getDimensions()).toEqual(sceneDimensions);
+  expect(engine.getScene()).toBe(scene);
   const viewport = engine.getViewport();
   expect(viewport.getDimensions()).toEqual(viewportDimensions);
 
